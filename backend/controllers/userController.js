8784b82ab18666d04165a3d9b8c43c633dbe525f
Travis Nguyen <trng2200@gmail.com>
2023-10-23 23:35:54 -0700

    remove unused var

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,6 @@
 const User = require('../models/userModel.js');
 const jwt = require('jsonwebtoken');
 
-const mongoose = require('mongoose');
-
 const createToken = (_id) => {
     return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
 };
