4920a78571e0f8ace7f32d162188fc55bd712cdc
kesdlvi <kesdlvi12@gmail.com>
2023-10-23 22:36:32 -0700

    Frontend added

---
+++ frontend/rea-list/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <h1>ReaList</h1>
+    // <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //     </Routes>
+    //   </BrowserRouter>
+  );
+}
+
+export default App;
