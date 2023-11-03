573d24d0749f44d8ef9566bc25e2fe6f4fc97ec5
kesdlvi <kesdlvi12@gmail.com>
2023-11-03 02:09:52 -0700

    Skeleton HomePage Created

--- frontend/rea-list/src/pages/Home.js
+++ frontend/rea-list/src/pages/Home.js
@@ -1,8 +1,34 @@
+import '../styles/HomePage.css'
+import Navbar from '../templates/Navbar'
+import Friends from './Friends'
+import Community from './Community'
+import { useState } from 'react'
 
 const Home = () => {
+    const [isCommunityClicked, setIsCommunityClicked] = useState(false)
+    const [isFriendsClicked, setIsFriendsClicked] = useState(true);
 
     return ( 
-        <h1>ReaList</h1>
+        
+        <div className="full-page">
+        
+        <Navbar 
+            isFriendsClicked={isFriendsClicked} 
+            setIsFriendsClicked={setIsFriendsClicked}
+            isCommunityClicked = {isCommunityClicked}
+            setIsCommunityClicked = {setIsCommunityClicked}
+        />
+            
+            {/* <-- Two pages for community and friends and statistics-> */}
+            <div className='page-content'>
+                {isFriendsClicked && <Friends></Friends>}
+                {isCommunityClicked && <Community></Community>}
+            </div>
+            {/* <div className='sidebar'>
+                <h2>SideBar will be here</h2>
+            </div> */}
+
+        </div>
     )
 }
 
