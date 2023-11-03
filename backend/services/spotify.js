0f06598b571cda7809c07970c59f03ef451650b7
Travis Nguyen <trng2200@gmail.com>
2023-11-03 16:24:03 -0700

    add more spotify feats.

--- backend/services/spotify.js
+++ backend/services/spotify.js
@@ -66,14 +66,76 @@ const getTop = async (token, refresh_token, type, limit, time_range) => {
         });
 };
 
+const getRecentlyPlayed = async (token, refresh_token, limit) => {
+    await fetch(
+        `https://api.spotify.com/v1/me/player/recently-played?limit=${limit}`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    )
+        .then(async (response) => {
+            if (response.status === 200) {
+                response.json().then((data) => {
+                    console.log(
+                        data.items.map((x) => x.track).map((x) => x.name)
+                    );
+                });
+            } else {
+                console.log(response);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.send(error);
+        });
+};
+
+const getNowPlaying = async (token, refresh_token) => {
+    await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then(async (response) => {
+            if (response.status === 200) {
+                response.json().then((data) => {
+                    console.log(data.item.name);
+                });
+            } else if (response.status === 204) {
+                getRecentlyPlayed(token, refresh_token, 1);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.send(error);
+        });
+};
+
 getTop(
-    'BQCH5ft8By5_C7fbNeAt_rx42s18QXtYTCAHVu_POgoQ8nfHa_8YLFY4LtQ8X2XPFuo7odoCHWQdqFmntxlM-FmjNPi5nBAeaEN-W1HSKP7eoNIR4lArwZ-KJN3C1kCYu7dhr67Bc7qgkLV0YNBW-pR_ACW1tDC-8G7mz8Hsu11IqRQQ9CkVzopFmDx7kYj5dnNrZUmYx8n-0ks0dQAGkQ',
-    'track',
+    'BQDuV2EkceX6_Wzz7ftjJBwMqVQuiI85yfoY_1Tke6qI31jDxPNx4vQEFqM42nr2HffzBFJjHGW9fAd_Bmhnf1pvBoQy0fob3dqWwX6YDeArobk0RC6MnSD-wf_EaoRAPgH4tKQMqv2U4Pkr_r301TiPXRNqt77eTnH8Od9ms4WXS1pvzCh8oy9Azq4P9xefqqWMYQBXgkBWDnr6K-O-dQ',
+    'AQB_M-Tledv7iCq5RCxkc0maxBH4KCP_LtOMSbCtVprTmYtRW95ZYyqTjZQu_50pvSZVMjrsXJlptEWrmEfJm-OwBroAl5KFGdx3Eq_W4q4fSAFMa_i7aZFHs3j0sh8qxlQ',
+    'tracks',
     '10',
     'long_term'
+);
+
+getRecentlyPlayed(
+    'BQDuV2EkceX6_Wzz7ftjJBwMqVQuiI85yfoY_1Tke6qI31jDxPNx4vQEFqM42nr2HffzBFJjHGW9fAd_Bmhnf1pvBoQy0fob3dqWwX6YDeArobk0RC6MnSD-wf_EaoRAPgH4tKQMqv2U4Pkr_r301TiPXRNqt77eTnH8Od9ms4WXS1pvzCh8oy9Azq4P9xefqqWMYQBXgkBWDnr6K-O-dQ',
+    'AQB_M-Tledv7iCq5RCxkc0maxBH4KCP_LtOMSbCtVprTmYtRW95ZYyqTjZQu_50pvSZVMjrsXJlptEWrmEfJm-OwBroAl5KFGdx3Eq_W4q4fSAFMa_i7aZFHs3j0sh8qxlQ',
+    '1'
+);
+
+getNowPlaying(
+    'BQDuV2EkceX6_Wzz7ftjJBwMqVQuiI85yfoY_1Tke6qI31jDxPNx4vQEFqM42nr2HffzBFJjHGW9fAd_Bmhnf1pvBoQy0fob3dqWwX6YDeArobk0RC6MnSD-wf_EaoRAPgH4tKQMqv2U4Pkr_r301TiPXRNqt77eTnH8Od9ms4WXS1pvzCh8oy9Azq4P9xefqqWMYQBXgkBWDnr6K-O-dQ',
+    'AQB_M-Tledv7iCq5RCxkc0maxBH4KCP_LtOMSbCtVprTmYtRW95ZYyqTjZQu_50pvSZVMjrsXJlptEWrmEfJm-OwBroAl5KFGdx3Eq_W4q4fSAFMa_i7aZFHs3j0sh8qxlQ'
 );
 
 module.exports = {
     refreshToken,
     getTop,
+    getNowPlaying,
 };
