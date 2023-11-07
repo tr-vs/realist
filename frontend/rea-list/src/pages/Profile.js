b5f61e8fd8f00c92c6dec4f5effdbce218426e1a
Travis Nguyen <trng2200@gmail.com>
2023-11-06 19:15:46 -0800

    useLogout Hook

--- frontend/rea-list/src/pages/Profile.js
+++ frontend/rea-list/src/pages/Profile.js
@@ -1,15 +1,24 @@
-import UserStats from "../templates/UserStats.js";
-import UserHead from "../templates/UserHead.js";
+import UserStats from '../templates/UserStats.js';
+import UserHead from '../templates/UserHead.js';
+import { useLogout } from '../hooks/useLogout.js';
+
 const Profile = () => {
+    const { logout } = useLogout();
+    const handleClick = () => {
+        logout();
+    };
+
     return (
         <div>
-            <UserHead/>
+            <UserHead />
+            <div>
+                <button onClick={handleClick}>Log Out</button>
+            </div>
             <a href="http://localhost:3000/api/spotify/auth">
                 <button>Connect to Spotify</button>
             </a>
-            <UserStats/>
+            <UserStats />
         </div>
-        
     );
 };
 
