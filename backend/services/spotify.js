b4656251e41a6d535813f3ee78b6d0fe7c0aa391
Jason Lozada <jasonlozada16@g.ucla.edu>
2023-11-08 16:19:42 -0800

    minor deletions of sample functions in spotify.js

--- backend/services/spotify.js
+++ backend/services/spotify.js
@@ -32,12 +32,8 @@ const refreshToken = async (refresh_token) => {
 };
 
 const getTop = async (token, refresh_token, type, limit, time_range) => {
-<<<<<<< Updated upstream
-    await fetch(
-=======
     
     const response = await fetch(
->>>>>>> Stashed changes
         `https://api.spotify.com/v1/me/top/${type}?limit=${limit}&time_range=${time_range}`,
         {
             method: 'GET',
@@ -46,37 +42,11 @@ const getTop = async (token, refresh_token, type, limit, time_range) => {
             },
         }
     )
-<<<<<<< Updated upstream
-        .then(async (response) => {
-            if (response.status === 200) {
-                response.json().then((data) => {
-                    console.log(data.items.map((x) => x.name));
-                    return data.items;
-                });
-            } else {
-                const errorHeader = response.headers.get('www-authenticate');
-                const strippedError = errorHeader.substring(
-                    errorHeader.indexOf('error_description="') +
-                        'error_description="'.length,
-                    errorHeader.length - 1
-                );
-
-                if (strippedError === 'The access token expired') {
-                    const refreshedToken = await refreshToken(refresh_token);
-                    console.log(refreshedToken);
-                }
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send(error);
-=======
       
     if (response.status === 200) {
         response.json().then((data) => {
             console.log(data.items.map((x) => x.name));
             return data.items;
->>>>>>> Stashed changes
         });
     } else {
         const errorHeader = response.headers.get('www-authenticate');
@@ -107,39 +77,12 @@ const getRecentlyPlayed = async (token, refresh_token, limit) => {
     if (response.status === 200) {
         response.json().then((data) => {
 
-<<<<<<< Updated upstream
-                    const tracks = data.items.map((x) => x.track).map((x) => x.name);
-                    const ids = data.items.map((x) => x.track).map((x) => x.id);
-                    // console.log(tracks[0]);
-                    // console.log(ids[0]);
-                    return data.items;
-                    getTrackImage(token, refresh_token, ids[0]);
-                });
-            } else {
-                const errorHeader = response.headers.get('www-authenticate');
-                const strippedError = errorHeader.substring(
-                    errorHeader.indexOf('error_description="') +
-                        'error_description="'.length,
-                    errorHeader.length - 1
-                );
-
-                if (strippedError === 'The access token expired') {
-                    const refreshedToken = await refreshToken(refresh_token);
-                    console.log(refreshedToken);
-                }
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send(error);
-=======
             const tracks = data.items.map((x) => x.track).map((x) => x.name);
             const ids = data.items.map((x) => x.track).map((x) => x.id);
             console.log(tracks[0]);
             console.log(ids[0]);
             return data.items;
 
->>>>>>> Stashed changes
         });
     } else {
         const errorHeader = response.headers.get('www-authenticate');
@@ -164,38 +107,10 @@ const getNowPlaying = async (token, refresh_token) => {
             Authorization: `Bearer ${token}`,
         },
     })
-<<<<<<< Updated upstream
-        .then(async (response) => {
-            if (response.status === 200) {
-                response.json().then((data) => {
-                    return data.items;
-                    getTrackImage(token, refresh_token, data.item.id);
-                });
-            } else if (response.status === 204) {
-                getRecentlyPlayed(token, refresh_token, 1);
-            }else {
-                const errorHeader = response.headers.get('www-authenticate');
-                const strippedError = errorHeader.substring(
-                    errorHeader.indexOf('error_description="') +
-                        'error_description="'.length,
-                    errorHeader.length - 1
-                );
-
-                if (strippedError === 'The access token expired') {
-                    const refreshedToken = await refreshToken(refresh_token);
-                    console.log(refreshedToken);
-                }
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.send(error);
-=======
     if (response.status === 200) {
         response.json().then((data) => {
             console.log(data.items);
             return data.items;
->>>>>>> Stashed changes
         });
     } else if (response.status === 204) {
         getRecentlyPlayed(token, refresh_token, 1);
@@ -284,25 +199,6 @@ const getTrackImage = async (token, refresh_token, id) => {
 }
 
 
-const TOKEN = "BQAdnB2rgwG7ufXVEcOWL0NX4OHSraSCWlVAdhscCKpVjwjkR5dtbRcBBVWtkyC8jBSc5QJ7wKlA5-LQYl1BGqskLqPv-b4C7hO0ac-n6n5KY35Slsn9f6HP31lkv5pWpXDW20PRaUXfGdcfIV_n5SfYdQza3TdS51K8tQJzbW89yflgJFODsxy5A6D9CpCgp_jUdFWcA8epCwoMVg"
-const REFRESH = "AQDU0F2TMA6PQRwtB9bZ3UwLzG-HP17lroGsd1HAyCe7GFZTcZbz32_fY2gO5za7cSFmd4e-jPCFyx4irFG7XVhokEyJEJczUUnV3Zz95a7KUFsJDFPhVFGhOUbVop5P0Vo"
-
-getTop(
- TOKEN, REFRESH, 'tracks',
-    '10',
-    'long_term'
-);
-
-getRecentlyPlayed(
-TOKEN, REFRESH,
-    '1'
-);
-
-getNowPlaying(
- TOKEN, REFRESH 
-);
-
-getUserProfilePic(TOKEN, REFRESH); 
 
 module.exports = {
     refreshToken,
