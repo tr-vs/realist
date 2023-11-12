d59444dca13ffbf6b57e2d6262da846d52ae0565
kesdlvi <kesdlvi12@gmail.com>
2023-11-12 15:49:53 -0800

    Adjusted light size

--- frontend/rea-list/src/templates/Posts.js
+++ frontend/rea-list/src/templates/Posts.js
@@ -7,7 +7,7 @@ const Posts = (props) => {
     const links = [
         'https://media.pitchfork.com/photos/5929c43cea9e61561daa80db/master/pass/a240bddc.jpg',
         'https://images.squarespace-cdn.com/content/v1/5e40c67d62402c0ce36a6bf0/1603566903470-375SZI5GD0F53P2LPGXR/Ef1eZcOX0AEmSs_.jpg',
-        'https://www.billboard.com/wp-content/uploads/2022/05/bad-bunny-cover-art-2022-billboard-1240.jpg?w=1024',
+        'https://upload.wikimedia.org/wikipedia/en/5/5e/Madvillainy_cover.png',
     ];
 
     return (
