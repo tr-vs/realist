77ed30dc3a25b38f35d73bf7032ba1f911dee772
Travis Nguyen <trng2200@gmail.com>
2023-11-13 21:41:19 -0800

    set up routes

--- backend/controllers/mainController.js
+++ backend/controllers/mainController.js
@@ -1,7 +1,17 @@
-const getNowPlaying = require('../services/spotify');
+const { getNowPlaying, getUserProfilePic } = require('../services/spotify');
 
 const home = async (req, res) => {
     console.log(getNowPlaying());
+};
+
+const profile = async (req, res) => {
+    const user = req.user;
+
+    if (user.access_token && user.refresh_token) {
+        // send spi call
+    } else {
+        res.status(401).json({ error: 'User not connected to Spotify!' });
+    }
 };
 
 module.exports = {
