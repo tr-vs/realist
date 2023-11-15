c8094f62dc2a6419858332448d232c90dac56caa
kesdlvi <kesdlvi12@gmail.com>
2023-11-15 03:40:07 -0800

    Updated SideBar style

--- frontend/rea-list/src/templates/SecondProfileIcon.js
+++ frontend/rea-list/src/templates/SecondProfileIcon.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SecondProfileIcon = ({onClick}) => {
     return (
         <Link to={'/profile'}>
-            <img onClick={onClick} className='second-profile-icon-image'src="https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png" alt="" /> 
+            <img onClick={onClick} className='second-profile-icon-image'src="https://i.scdn.co/image/ab67706c0000da842a6199fd8dcd31ca3eadfd17" alt="" /> 
         </Link>
     ); 
 };
