7ffcf716a6ee3de75361a4e7bcbadc83b5a7f149
kesdlvi <kesdlvi12@gmail.com>
2023-11-27 00:47:42 -0800

    Updated default profile photo

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -38,7 +38,7 @@ const userSchema = new Schema({
     pfp: {
         type: String,
         default:
-            'https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png',
+            'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg',
     },
 });
 
