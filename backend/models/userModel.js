e3aa9b2e00f2db09872f94d67ed4ba1a866ea8e1
Travis Nguyen <trng2200@gmail.com>
2023-11-29 12:41:31 -0800

    update schema to hold following/follower arrays

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -47,6 +47,8 @@ const userSchema = new Schema({
                 'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg',
         },
     ],
+    followers: [{ type: String }],
+    following: [{ type: String }],
 });
 
 // static signup method
