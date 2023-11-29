54d513ec8c7935d9891c3f57882ecd19ba248111
Jacqueline Nguyen <jacquelinenguyen@MacBook-Air-2.local>
2023-11-28 23:26:06 -0800

    making a user not found page

---
+++ frontend/rea-list/src/pages/UserNotFound.js
@@ -0,0 +1,17 @@
+const UserNotFound = () => {
+
+
+    const userNotFound = () => {
+        return (
+          <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>User Not Found</h1>
+            <p>We couldn't find the requested user.</p>
+          </div>
+        );
+      };
+ }
+ 
+ 
+ export default UserNotFound;
+ 
+ 
