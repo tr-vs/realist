1f38a2c03306492733612f6405bfe02ae6be8801
Teresa <teresalee2@g.ucla.edu>
2023-12-04 20:02:45 -0800

    Styled login page

--- frontend/rea-list/src/templates/SignIn.js
+++ frontend/rea-list/src/templates/SignIn.js
@@ -92,7 +92,7 @@ export default function SignIn() {
                 <CssBaseline />
                 <Box
                     sx={{
-                        marginTop: 8,
+                        marginTop: 14,
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
