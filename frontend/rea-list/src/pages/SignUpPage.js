591b24814a32b5b8edec238e9e2f31d3a4b95557
Travis Nguyen <trng2200@gmail.com>
2023-12-04 21:57:51 -0800

    transfer passkeys signup element to old component

--- frontend/rea-list/src/pages/SignUpPage.js
+++ frontend/rea-list/src/pages/SignUpPage.js
@@ -1,48 +1,12 @@
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useEffect, useRef } from 'react';
 import '@passageidentity/passage-elements/passage-register';
+import SignUp from '../templates/SignUp';
 
 const SignUpPage = () => {
-    const ref = useRef();
-    const { dispatch, user } = useAuthContext();
-
-    const onSuccess = async (authResult) => {
-        const response = await fetch(
-            process.env.REACT_APP_BACKEND + 'api/users/signup',
-            {
-                method: 'POST',
-                credentials: 'include',
-                body: JSON.stringify({ username: user.username }),
-                headers: {
-                    Authorization: `Bearer ${authResult.auth_token}`,
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
-        const json = await response.json();
-
-        if (response.ok) {
-            // save user to local storage
-            localStorage.setItem('user', JSON.stringify(json));
-
-            // update the auth context
-            dispatch({ type: 'LOGIN', payload: json });
-        }
-    };
-
-    useEffect(() => {
-        const { current } = ref;
-        current.onSuccess = onSuccess;
-        return () => {};
-    });
-
     return (
         <div className="full-container">
-            <passage-register
-                ref={ref}
-                onSuccess={onSuccess}
-                app-id={process.env.REACT_APP_PASSAGE_APP_ID}
-            ></passage-register>
+            <SignUp />
         </div>
     );
 };
