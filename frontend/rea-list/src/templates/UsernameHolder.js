462c760e29dc3f7a71c532d3e5e011b916a7510e
Travis Nguyen <trng2200@gmail.com>
2023-12-05 18:38:09 -0800

    force reload if searchin in otherprofile component

--- frontend/rea-list/src/templates/UsernameHolder.js
+++ frontend/rea-list/src/templates/UsernameHolder.js
@@ -1,35 +1,31 @@
-import ProfileIcon from "./ProfileIcon"
+import ProfileIcon from './ProfileIcon';
 import '../styles/UsernameHolder.css';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const UsernameHolder = ({pfp, username})=> {
+const UsernameHolder = ({ pfp, username }) => {
     const imagesrc =
         pfp !== undefined
             ? pfp
             : 'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg';
 
-    const usernameSrc = 
-        username !== undefined  
-            ? username
-            : "kesdlvi"
+    const usernameSrc = username !== undefined ? username : 'kesdlvi';
 
     const handleClick = () => {
         window.location.reload();
-    }
+    };
 
     return (
-        <Link to={ `/${usernameSrc}` }>
+        <Link reloadDocument to={`/${usernameSrc}`}>
             <div className="username-pfp-container">
-                <img 
+                <img
                     className="search-menu-pfp"
                     src={imagesrc}
-                    alt="profile-photo" 
+                    alt="profile-photo"
                 />
-                <h3 className='username-input'>{usernameSrc}</h3>
-                
+                <h3 className="username-input">{usernameSrc}</h3>
             </div>
         </Link>
-    )
-}
+    );
+};
 
-export default UsernameHolder
+export default UsernameHolder;
