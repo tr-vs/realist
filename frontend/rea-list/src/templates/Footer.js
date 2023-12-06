dd8bce54bb12f8e65952a3f25d958c18393ca184
Teresa <teresalee2@g.ucla.edu>
2023-12-05 17:34:52 -0800

    Implemented and edited footer for home page

--- frontend/rea-list/src/templates/Footer.js
+++ frontend/rea-list/src/templates/Footer.js
@@ -7,23 +7,22 @@ const Footer = () => {
                 <div>
                     <h1 className="footer-list-title">Support</h1>
                     <ul className="footer-list">
-                        <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> About </a></li>
-                        <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> Donate </a></li>
+                        <li className="footer-list-item"> <a href = "https://docs.google.com/presentation/d/17s7EROZYcndUFGXO3eVGDJ2ufHWQGr0iAtz2lyWuZ_g/edit?usp=sharing" className="footer-list-item"> About Us</a></li>
+                        <li className="footer-list-item"> <a href = "https://www.buymeacoffee.com/realistdonation" className="footer-list-item"> Donate </a></li>
                     </ul>
                 </div>
                 <div>
                     <h1 className="footer-list-title">Contact</h1>
                     <ul className="footer-list">
                         <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> Location </a></li>
-                        <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> Email </a></li>
-                        <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> Give Us Feedback </a></li>
+                        <li className="footer-list-item"> <a href= "mailto:support@realist.top" className="footer-list-item"> Email </a></li>
                     </ul>
                 </div>
                 <div>
                     <h1 className="footer-list-title">Socials</h1>
                     <ul className="footer-list">
-                        <li className="footer-list-item"> <a href = "https://github.com/tr-vs/realist" className="footer-list-item"> GitHub </a></li>
-                        <li className="footer-list-item"> <a href = "https://www.ucla.edu/" className="footer-list-item"> Our LinkedIns </a></li>
+                        <li className="footer-list-item"> <a href = "https://github.com/tr-vs/realist" className="footer-list-item"> Our Code </a></li>
+                        <li className="footer-list-item"> <a href = "https://linktr.ee/realistlinktree" className="footer-list-item"> Our GitHub </a></li>
                     </ul>
                 </div>
             </div>
